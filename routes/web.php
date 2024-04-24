c926db610d73e4d0e884ff05ee21471c41def1b4
Rajhaga <hagaraj7@gmail.com>
2024-04-24 21:34:33 +0700

    layout update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/home', function () {
+    return view('index');
+});
+
+Route::get('/aboutus', function () {
+    return view('about');
+});
+
+Route::get('/package', function () {
+    return view('packages');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
